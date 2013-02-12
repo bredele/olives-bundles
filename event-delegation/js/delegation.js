d4db7268172f1c39606953f5a6bf756c5fb9a485
bredele <olivier.wietrich@gmail.com>
2013-02-12 17:16:18 +0100

    Event delegation modernizr

--- event-delegation/js/delegation.js
+++ event-delegation/js/delegation.js
@@ -1,6 +1,6 @@
 require(["OObject", "Event.plugin"], function(Widget, Event){
 	var widget = new Widget();
-	widget.plugins.add("event", new Event(widget));
+	widget.plugins.add("event", new Event(widget, document.documentElement.classList.contains("touch")));
 	widget.alert = function(event){
 		alert( event.target.innerText + " has been fired");
 	};
